e5e1c08f6ca4ddba98021dd1529ee1f31713a5f4
hongjianqiang <569250030@qq.com>
2019-11-05 22:38:55 +0800

    移除了没有用到的包

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,7 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const package = require('../package.json');
 
 module.exports = merge(common, {
     mode: 'production'
