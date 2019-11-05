229fc517ef85bffa0a5c1bc0e0e2ea6848970f59
hongjianqiang <569250030@qq.com>
2019-11-05 22:38:13 +0800

    build后不生成用于Demo的HTML文件

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -4,20 +4,5 @@ const common = require('./webpack.common.js');
 const package = require('../package.json');
 
 module.exports = merge(common, {
-    mode: 'production',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: package.description,
-            template: './src/index.html',
-            filename: './index.html',
-            inject: 'body',
-            minify: true
-        }),
-    ],
-    performance: {
-        // assetFilter(assetFilename) {
-            // 排除 .map .lock .json 文件的性能提示
-            // return !(/\.(map|lock|json)$/.test(assetFilename));
-        // }
-    }
+    mode: 'production'
 });
