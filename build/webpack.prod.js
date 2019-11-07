cb98b8712f70c57a7a25d9800d72e989f5dabfc1
hongjianqiang <569250030@qq.com>
2019-11-07 16:36:55 +0800

    修改了build生成Demo路径

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,6 +1,17 @@
+const package = require('../package.json');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: package.description,
+            template: './src/index.html',
+            filename: '../demo/index.html',
+            inject: 'body',
+            minify: true
+        }),
+    ]
 });
